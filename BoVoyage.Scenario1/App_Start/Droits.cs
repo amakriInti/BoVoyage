d9d479706f5166bc30dc9946d6cd186fb93a6d75
MAKRI <31958699+alimakri@users.noreply.github.com>
2021-06-10 17:43:29 +0200

    Ajoutez des fichiers projet.

---
+++ BoVoyage.Scenario1/App_Start/Droits.cs
@@ -0,0 +1,36 @@
+﻿using BoVoyage.Scenario1.Dal;
+using BoVoyage.Scenario1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace BoVoyage.Scenario1
+{
+    public static class Droits
+    {
+        private static Repository Repo = new Repository();
+        internal static void Load()
+        {
+            // aspnet_regsql.exe -S .\SQLEXPRESS -E -A all -d BoVoyage
+
+            // Mettre dans web.config dans sytem.web :     
+            //< roleManager enabled = "true" cacheRolesInCookie = "true" cookieName = ".ASPROLES" defaultProvider = "DefaultRoleProvider" >
+            //         < providers >
+            //           < add connectionStringName = "BoVoyage" applicationName = "/" name = "DefaultRoleProvider" type = "System.Web.Security.SqlRoleProvider" />
+            //                </ providers >
+            //              </ roleManager >
+
+            if (!Roles.RoleExists(StatutEnum.Commercial.ToString())) Roles.CreateRole(StatutEnum.Commercial.ToString());
+            if (!Roles.RoleExists(StatutEnum.Admin.ToString())) Roles.CreateRole(StatutEnum.Admin.ToString());
+            if (!Roles.IsUserInRole("ali@bovoyage.com", StatutEnum.Admin.ToString())) Roles.AddUserToRole("ali@bovoyage.com", StatutEnum.Admin.ToString());
+
+            var mails = Repo.GetAllMails(StatutEnum.Commercial);
+            foreach (var mail in mails)
+            {
+                if (!Roles.IsUserInRole(mail, StatutEnum.Commercial.ToString())) Roles.AddUserToRole(mail, StatutEnum.Commercial.ToString());
+            }
+        }
+    }
+}
---
+++ BoVoyage.Scenario1/Dal/Repository.cs
@@ -0,0 +1,20 @@
+﻿using BoVoyage.Scenario1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyage.Scenario1.Dal
+{
+    public class Repository
+    {
+        private BoVoyageContext Context = new BoVoyageContext();
+        internal List<string> GetAllMails(StatutEnum statut)
+        {
+            return Context.Employes
+                .Where(e => (StatutEnum)e.Statut == statut)
+                .Select(e => e.Login)
+                .ToList();
+        }
+    }
+}
