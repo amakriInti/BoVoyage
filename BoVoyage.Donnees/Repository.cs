ecb894eabe63b7000a6810e36f1fbdc885679274
kfrances-inti <krystal.ml.frances@gmail.com>
2021-06-14 11:09:18 +0200

    11h09

---
+++ BoVoyage.Donnees/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage.Donnees
+{
+    public class Repository
+    {
+        private BoVoyageContext Context = new BoVoyageContext();
+        internal List<string> GetAllMails(StatutEnum statut)
+        {
+            var liste = Context.Employes.ToList();
+            return liste
+                .Where(e => (StatutEnum)e.Statut == statut)
+                .Select(e => e.Login)
+                .ToList();
+        }
+    }
+}
---
+++ BoVoyage.Metier/ClassMetier.cs
@@ -0,0 +1,15 @@
+﻿using BoVoyage.Donnees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage.Metier
+{
+    public class ClassMetier
+    {
+        private Repository repository = new Repository();
+
+    }
+}
