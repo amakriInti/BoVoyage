95d50d70f7d4e2a185aaea3a6043c2cf336dd9ac
Striegel <diego.striegel@gmail.com>
2021-06-15 16:30:13 +0200

    quentin est un .....

--- BoVoyage.Donnees/Repository.cs
+++ BoVoyage.Donnees/Repository.cs
@@ -76,4 +76,5 @@ namespace BoVoyage.Donnees
             }
         }
     }
+
 }
--- BoVoyage.Metier/ClassMetier.cs
+++ BoVoyage.Metier/ClassMetier.cs
@@ -11,6 +11,44 @@ namespace BoVoyage.Metier
     public class ClassMetier
     {
         private Repository repository = new Repository();
+        public bool AddVoyage()
+        {
+            // Lecture du fichier csv -> ps
+            //Récupère tous les fichier .csv du répertoir et le met dans un tableau de string
+            string[] files = Directory.GetFiles(@"D:\formation .net\Module C#\DemoASP\test_voyage\BoVoyage.Scenario1\Voyage_csv", "*.csv");
+
+            //boucle pour récuperer le fichier spécifique du tableau de string
+            for (int i = 0; i < files.Count(); i++)
+            {
+                //boucle pour intégrer les données du fichier csv dans la BDD
+                foreach (var ligne in File.ReadAllLines(files[i]))
+                {
+                    try
+                    {
+                        var tab = ligne.ToString().Split(';');
+                        bool CtrlChanges = repository.AddVoyage(tab);
+                        if (CtrlChanges == false)
+                        {
+                            throw new Exception();
+                        }
+                    }
+                    catch(Exception)
+                    {
+                        return false;
+                    }
+                    
+                }
+                 //supprime un fichier arès l'avoir lu
+                File.Delete(files[i]);
+            }
+            return true;
+        }
+
+        public object DBVoyages()
+        {
+            return repository.DBVoyages();
+        }
+
         private Droits droits = new Droits();
 
         /*----------------------------------
