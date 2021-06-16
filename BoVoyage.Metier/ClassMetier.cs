78aa0e367bc5d1e0f146db439e86a40b0fcbea69
quentin lefevre <cantine5186@hotmail.fr>
2021-06-16 11:03:30 +0200

    VersionAlpha 0.0.1.7

--- BoVoyage.Metier/ClassMetier.cs
+++ BoVoyage.Metier/ClassMetier.cs
@@ -12,90 +12,43 @@ namespace BoVoyage.Metier
     {
         private Repository repository = new Repository();
         private Droits droits = new Droits();
-        public bool AddVoyage()
-        {
-            // Lecture du fichier csv -> ps
-            //Récupère tous les fichier .csv du répertoir et le met dans un tableau de string
-            string[] files = Directory.GetFiles(@"D:\formation .net\Module C#\DemoASP\SaveVoyage\BoVoyage.Scenario1\Voyage_csv", "*.csv");
 
-            //boucle pour récuperer le fichier spécifique du tableau de string
+        /*----------------------------------
+        //Ajout des voyages depuis fichier CSV
+        -----------------------------------*/
+        public bool AddVoyageCSV_Metier()
+        {
+            //Lecture du fichier csv -> ps
+            //Récupère tous les fichier .csv du répertoire et le met dans un tableau de string
+            string[] files = Directory.GetFiles(@"D:\Utilisateurs\LEFEVRE Quentin\Documents\Cour\Cour_INTI\Projet Fin Formation\Application\BoVoyage.Scenario1\Voyage_csv\", "*.csv");
+
+            //boucle sur les éléments du tableau de fichier
             for (int i = 0; i < files.Count(); i++)
             {
-                //boucle pour intégrer les données du fichier csv dans la BDD
+                //Lecture et ajout dans la DB
                 foreach (var ligne in File.ReadAllLines(files[i]))
                 {
                     try
                     {
                         var tab = ligne.ToString().Split(';');
+                        //Ajout dans la DB
                         bool CtrlChanges = repository.AddVoyage(tab);
                         if (CtrlChanges == false)
                         {
                             throw new Exception();
                         }
                     }
-                    catch(Exception ex)
+                    catch(Exception)
                     {
                         return false;
                     }
                     
                 }
-                 //supprime un fichier arès l'avoir lu
+                //Suppression du fichier
                 File.Delete(files[i]);
             }
             return true;
         }
-
-        /*----------------------------------
-        //Afficher liste des voyages
-        -----------------------------------*/
-        public object DBVoyages(string tri, string choix)
-        {
-            return repository.DBVoyages(tri, choix);
-        }
-
-        public object DetailsVoyage(string id)
-        {
-            return repository.DetailsVoyage(id);
-        }
-
-
-
-        ///*----------------------------------
-        ////Ajout des voyages depuis fichier CSV
-        //-----------------------------------*/
-        //public bool AddVoyageCSV_Metier()
-        //{
-        //    //Lecture du fichier csv -> ps
-        //    //Récupère tous les fichier .csv du répertoire et le met dans un tableau de string
-        //    string[] files = Directory.GetFiles(@"D:\Utilisateurs\LEFEVRE Quentin\Documents\Cour\Cour_INTI\Projet Fin Formation\Application\BoVoyage.Scenario1\Voyage_csv\", "*.csv");
-
-        //    //boucle sur les éléments du tableau de fichier
-        //    for (int i = 0; i < files.Count(); i++)
-        //    {
-        //        //Lecture et ajout dans la DB
-        //        foreach (var ligne in File.ReadAllLines(files[i]))
-        //        {
-        //            try
-        //            {
-        //                var tab = ligne.ToString().Split(';');
-        //                //Ajout dans la DB
-        //                bool CtrlChanges = repository.AddVoyage(tab);
-        //                if (CtrlChanges == false)
-        //                {
-        //                    throw new Exception();
-        //                }
-        //            }
-        //            catch(Exception)
-        //            {
-        //                return false;
-        //            }
-
-        //        }
-        //        //Suppression du fichier
-        //        File.Delete(files[i]);
-        //    }
-        //    return true;
-        //}
 
         /*----------------------------------
         //Ajout des voyages depuis formulaire
